8d674cc7a80c3de77e7bd052295dcb6b68beed9c
Ty Chermsirivatana <professornitro@gmail.com>
2020-09-27 17:21:21 -0400

    Added some misc changes

--- AdminPanel/adminpanel.py
+++ AdminPanel/adminpanel.py
@@ -1,4 +1,5 @@
 import os
+import io
 import socket
 import sys
 import requests  
--- AdminPanel/apfunc.py
+++ AdminPanel/apfunc.py
@@ -1,5 +1,12 @@
+import io
+import os
+import socket
+import sys
+import time
+import requests  
+
 # This is the function array that can be called into the main file (adminpanel.py) 
 
 #This is a example function
 def systemsupport():
-  print("SYSTEM")
+  print("SYSTEM") 
