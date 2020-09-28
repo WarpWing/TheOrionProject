606ffdf67db2a00c5a51c09708e64a506af8d1a9
Ty Chermsirivatana <professornitro@gmail.com>
2020-09-27 21:23:47 -0400

    Npyscreen migration

--- AdminPanel/adminpanel.py
+++ AdminPanel/adminpanel.py
@@ -1,7 +1,19 @@
-import django
+import npyscreen
+import json
 import os
 import sys
 import requests  
 from apfunc import * 
 
-systemsupport()
+class sop(npyscreen.NPSApp):
+    def main(self):
+        # This reference the main GUI and can call functions from apfunc
+        F  = npyscreen.Form(name = "Welcome to the Sentinel Overwatch Program",) 
+        dt = F.add(npyscreen.TitleDateCombo, name = "Date:")
+        
+        F.add(npyscreen.TitleText, name="Placeholder Text")
+        F.edit()
+
+if __name__ == "__main__":
+    App = sop()
+    App.run()
