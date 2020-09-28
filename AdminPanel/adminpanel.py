7183830253cb1d2a0ca77bd845497bc844f75c2a
Ty Chermsirivatana <professornitro@gmail.com>
2020-09-27 21:28:53 -0400

    Whitespace Sync

--- AdminPanel/adminpanel.py
+++ AdminPanel/adminpanel.py
@@ -10,7 +10,6 @@ class sop(npyscreen.NPSApp):
         # This reference the main GUI and can call functions from apfunc
         F  = npyscreen.Form(name = "Welcome to the Sentinel Overwatch Program",) 
         dt = F.add(npyscreen.TitleDateCombo, name = "Date:")
-        
         F.add(npyscreen.TitleText, name="Placeholder Text")
         F.edit()
 
