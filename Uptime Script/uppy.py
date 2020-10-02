f06ee9d16af81ad3baab03a61b22ed9c98853b4a
Ty Chermsirivatana <professornitro@gmail.com>
2020-10-02 10:31:32 -0400

    Added to uppy and Changed File Type for Hypixel API

--- Uptime Script/uppy.py
+++ Uptime Script/uppy.py
@@ -2,9 +2,6 @@ import os
 import requests
 import json 
 
-weblist = ["google.com", "sky.lea.moe"]
-
-for x in weblist:
-   r = requests.get('https://sky.shiiyu.moe')
-   rcode = r.status_code
-printf("The Response Code is {rcode} and it's beautiful")
+link = "https://google.com"
+r = requests.get(link)
+print("The response is {rcode} and I find it to be epic".format(rcode=r.status_code))
